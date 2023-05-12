f4ae6d5a0118134663691ede3eb0cca7cd22e40c
cmadrid48 <cmadrid48@gmail.com>
2023-05-11 19:54:44 -0700

    added components

--- reactPortfolio/src/App.jsx
+++ reactPortfolio/src/App.jsx
@@ -1,23 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Header />
       <h1>Vite + React</h1>
       <div className="card">
+        <Content />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,11 +20,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
