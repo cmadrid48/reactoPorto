82890bfd7edf4cf391620daca03b170100de0ca3
cmadrid48 <cmadrid48@gmail.com>
2023-05-11 21:09:16 -0700

    styles not working still,but added to header

--- reactPortfolio/src/components/Header.jsx
+++ reactPortfolio/src/components/Header.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div>
+      <nav>
+        <div class="logo">
+          folio<b>.</b>
+        </div>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Skills</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
